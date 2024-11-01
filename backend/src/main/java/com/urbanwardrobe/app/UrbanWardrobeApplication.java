2f777d5ed2eee3f1883b5dc3e2e60321d4a7327a
aasimalikhan <190010001@alum23.iitdh.ac.in>
2024-11-01 14:27:08 +0530

    BACKEND - boilerplate code added

---
+++ backend/src/main/java/com/urbanwardrobe/app/UrbanWardrobeApplication.java
@@ -0,0 +1,13 @@
+package com.urbanwardrobe.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrbanWardrobeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrbanWardrobeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/urbanwardrobe/app/UrbanWardrobeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.urbanwardrobe.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrbanWardrobeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
