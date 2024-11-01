881f604f445b4cfb2246db6c2ff3ce4f5e07fa6a
aasimalikhan <190010001@alum23.iitdh.ac.in>
2024-11-01 19:49:25 +0530

    BACKEND - Spring Security configuration added

---
+++ backend/src/main/java/com/urbanwardrobe/app/config/JwtConstant.java
@@ -0,0 +1,6 @@
+package com.urbanwardrobe.app.config;
+
+public class JwtConstant {
+    public static final String SECRET_KEY = "novorepnoye";
+    public static final String JWT_HEADER = "Authorization";
+}
